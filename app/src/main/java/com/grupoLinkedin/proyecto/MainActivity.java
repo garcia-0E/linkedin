4ce7b76b2890c2d210ffcbc91f9edba7c1cc0290
Jhonni Calle <jhonnicalle@gmail.com>
2019-03-05 07:46:46 -0400

    Agregando 'inicio de sesion' por la app

--- app/src/main/java/com/grupoLinkedin/proyecto/MainActivity.java
+++ app/src/main/java/com/grupoLinkedin/proyecto/MainActivity.java
@@ -1,13 +1,90 @@
 package com.grupoLinkedin.proyecto;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.linkedin.platform.APIHelper;
+import com.linkedin.platform.DeepLinkHelper;
+import com.linkedin.platform.LISessionManager;
+import com.linkedin.platform.errors.LIApiError;
+import com.linkedin.platform.errors.LIAuthError;
+import com.linkedin.platform.errors.LIDeepLinkError;
+import com.linkedin.platform.listeners.ApiListener;
+import com.linkedin.platform.listeners.ApiResponse;
+import com.linkedin.platform.listeners.AuthListener;
+import com.linkedin.platform.listeners.DeepLinkListener;
+import com.linkedin.platform.utils.Scope;
+import com.squareup.picasso.Picasso;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import static com.linkedin.platform.LISessionManager.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    String TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        getPackageHash();
+
+        LISessionManager.getInstance(getApplicationContext()).init(this, buildScope()//pass the build scope here
+                , new AuthListener() {
+                    @Override
+                    public void onAuthSuccess() {
+                        // Authentication was successful. You can now do
+                        // other calls with the SDK.
+                        Toast.makeText(MainActivity.this, "Successfully authenticated with LinkedIn.", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onAuthError(LIAuthError error) {
+                        // Handle authentication errors
+                        Log.e(TAG, "Auth Error :" + error.toString());
+                        Toast.makeText(MainActivity.this, "Failed to authenticate with LinkedIn. Please try again.", Toast.LENGTH_SHORT).show();
+                    }
+                }, true);//if TRUE then it will show dialog if
+        // any device has no LinkedIn app installed to download app else won't show anything
+
     }
+
+    private static Scope buildScope() {
+        return Scope.build(Scope.R_BASICPROFILE, Scope.W_SHARE);
+    }
+
+
+
+//    private void getPackageHash() {
+//        try {
+//            PackageInfo info = getPackageManager().getPackageInfo(
+//                    "com.grupoLinkedin.proyecto",//give your package name here
+//                    PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//
+//                Log.d(TAG, "Hash  : " + Base64.encodeToString(md.digest(), Base64.NO_WRAP));//Key hash is printing in Log
+//            }
+//        } catch (PackageManager.NameNotFoundException e) {
+//            Log.d(TAG, e.getMessage(), e);
+//        } catch (NoSuchAlgorithmException e) {
+//            Log.d(TAG, e.getMessage(), e);
+//        }
+//    }
 }
